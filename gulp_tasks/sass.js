6ed1633ecfd287fab3bb7607c53510c4cd06f83c
Ed Heltzel <ed@rainyday.media>
2020-05-08 14:52:33 -0400

    adds sourcemaps

--- gulp_tasks/sass.js
+++ gulp_tasks/sass.js
@@ -3,11 +3,17 @@ const config = require('../flightdeck.manifest.js');
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
 
 gulp.task('sass', function() {
   return gulp
-    .src(config.assets + '/' + config.sass.src + '/**/*')
-    .pipe(sass({ outputStyle: config.sass.outputStyle }).on('error', sass.logError))
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(gulp.dest(config.assets + '/' + config.sass.dest));
+  .src(config.assets + '/' + config.sass.src + '/**/*')
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: config.sass.outputStyle }).on('error', sass.logError))
+  .pipe(sourcemaps.write({includeContent: false}))
+  .pipe(sourcemaps.init({loadMaps: true}))
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(config.assets + '/' + config.sass.dest));
 });
