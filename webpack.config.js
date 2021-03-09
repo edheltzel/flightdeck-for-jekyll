739ee4b4d2aeb9bb9e56c50eacc9664df7ce7a16
Ed Heltzel <ed@rainyday.media>
2021-03-09 15:08:16 -0500

    fixes an issue where webpack failes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
-const config = require('./flightdeck.manifest.js');
+const config = require("./flightdeck.manifest.js")
 const path = require("path");
+
 module.exports = {
-  mode: config.webpack.mode,
+  mode: "production",
   output: {
     filename: "bundle.js"
   },
-  module: config.webpack.module.rules
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: [path.resolve(__dirname, config.assets + config.js.dest )],
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
