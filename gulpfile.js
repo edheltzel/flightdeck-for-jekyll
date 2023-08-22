8deb1afa90ef6534ebee09a4bcc8882dd94ce039
Ed Heltzel <ed@rainyday.media>
2023-08-22 14:25:11 -0400

    removes lint from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
-const config = require("./flightdeck.manifest");
+const config = require("./flightdeck.manifest")
 // Load plugins
-const { dest, src, watch, series, parallel } = require("gulp");
-const argv = require("yargs").argv;
-const autoprefixer = require("autoprefixer");
-const browsersync = require("browser-sync").create();
-const cp = require("child_process");
-const cssnano = require("cssnano");
-const del = require("del");
-const eslint = require("gulp-eslint");
-const imagemin = require("gulp-imagemin");
-const newer = require("gulp-newer");
-const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const sass = require("gulp-dart-sass");
-const terser = require("gulp-terser");
+const { dest, src, watch, series, parallel } = require("gulp")
+const argv = require("yargs").argv
+const autoprefixer = require("autoprefixer")
+const browsersync = require("browser-sync").create()
+const cp = require("child_process")
+const cssnano = require("cssnano")
+const del = require("del")
+const imagemin = require("gulp-imagemin")
+const newer = require("gulp-newer")
+const plumber = require("gulp-plumber")
+const postcss = require("gulp-postcss")
+const sass = require("gulp-dart-sass")
+const terser = require("gulp-terser")
 
-const buildDest = `${config.jekyll.dest}/assets/`;
+const buildDest = `${config.jekyll.dest}/assets/`
 
 // BrowserSync
 function browserSync(done) {
@@ -25,20 +24,20 @@ function browserSync(done) {
     },
     port: config.port,
     notify: config.bs.notify,
-  });
-  done();
+  })
+  done()
 }
 
 // BrowserSync Reload
 function browserSyncReload(done) {
-  browsersync.notify("🛠 Site Rebuilt", 1000);
-  browsersync.reload();
-  done();
+  browsersync.notify("🛠 Site Rebuilt", 1000)
+  browsersync.reload()
+  done()
 }
 
 // Clean assets
 function cleanAssets() {
-  return del(buildDest);
+  return del(buildDest)
 }
 
 // Optimize Images
@@ -57,7 +56,7 @@ function images() {
         { verbose: config.imagemin.verbose },
       ),
     )
-    .pipe(dest(buildDest + config.imagemin.dest));
+    .pipe(dest(buildDest + config.imagemin.dest))
 }
 
 // CSS task
@@ -68,16 +67,7 @@ function css() {
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest(buildDest + config.sass.dest, { sourcemaps: "." }))
     .pipe(dest("assets/css"))
-    .pipe(browsersync.stream());
-}
-
-// Lint scripts
-function scriptsLint() {
-  return src([config.assets + config.js.src, "./gulpfile.js", "!node_modules/**", "!*.map*"])
-    .pipe(plumber())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    .pipe(browsersync.stream())
 }
 
 // Transpile, concatenate and minify scripts
@@ -86,42 +76,42 @@ function scripts() {
     .pipe(plumber())
     .pipe(terser())
     .pipe(dest(buildDest + config.js.dest, { sourcemaps: "." }))
-    .pipe(browsersync.stream());
+    .pipe(browsersync.stream())
 }
 
 // Jekyll
 function jekyll(done) {
-  let jekyllConfig = config.jekyll.config.default;
+  let jekyllConfig = config.jekyll.config.default
   if (argv.jekyllEnv === "production") {
-    process.env.JEKYLL_ENV = "production";
-    jekyllConfig += `${config.jekyll.config.production ? `,${config.jekyll.config.production}` : ""}`;
+    process.env.JEKYLL_ENV = "production"
+    jekyllConfig += `${config.jekyll.config.production ? `,${config.jekyll.config.production}` : ""}`
   } else {
-    jekyllConfig += `${config.jekyll.config.development ? `,${config.jekyll.config.development}` : ""}`;
+    jekyllConfig += `${config.jekyll.config.development ? `,${config.jekyll.config.development}` : ""}`
   }
   return cp
     .spawn("bundle", ["exec", "jekyll", "build", "--config", jekyllConfig], { stdio: "inherit" })
-    .on("close", done);
+    .on("close", done)
 }
 
 // Watch files
 function watchFiles() {
-  watch(config.assets + config.sass.src, css);
-  watch(config.assets + config.js.src, series(scriptsLint, scripts));
-  watch(config.jekyll.watch, series(jekyll, browserSyncReload));
-  watch(config.assets + config.imagemin.src, series(images, jekyll, browserSyncReload));
+  watch(config.assets + config.sass.src, css)
+  watch(config.assets + config.js.src, series(scripts))
+  watch(config.jekyll.watch, series(jekyll, browserSyncReload))
+  watch(config.assets + config.imagemin.src, series(images, jekyll, browserSyncReload))
 }
 
 // define complex tasks
-const js = series(scripts);
-const build = series(cleanAssets, parallel(jekyll, css, js, images));
-const monitor = parallel(watchFiles, browserSync);
+const js = series(scripts)
+const build = series(cleanAssets, parallel(jekyll, css, js, images))
+const monitor = parallel(watchFiles, browserSync)
 
 // export tasks
-exports.images = images;
-exports.css = css;
-exports.js = js;
-exports.jekyll = jekyll;
-exports.cleanAssets = cleanAssets;
-exports.build = build;
-exports.monitor = monitor;
-exports.default = series(build, monitor);
+exports.images = images
+exports.css = css
+exports.js = js
+exports.jekyll = jekyll
+exports.cleanAssets = cleanAssets
+exports.build = build
+exports.monitor = monitor
+exports.default = series(build, monitor)
